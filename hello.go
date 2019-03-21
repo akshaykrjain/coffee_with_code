46121ca684468ee51b81cbe2b7bf91b165b36882
Akshay Jain <akshaykrjain@users.noreply.github.com>
2019-03-21 19:06:07 +0100

    Create hello.go

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	fmt.Println("Service running and listening on port :8080")
+	panic(http.ListenAndServe(":8080", nil))
+}
