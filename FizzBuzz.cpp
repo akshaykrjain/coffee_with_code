a59aae8c4f9d5549a19ca3fca8fe6df6f8cb2e81
Akshay Jain <akshaykrjain@users.noreply.github.com>
2019-05-18 16:43:14 +0200

    Corrected Formatting

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -25,17 +25,16 @@ int main()
 
 void CheckAndShowFizzBuzz(int i)
 {
-    
-    if(i%3==0 && i%5==0)
-        printf("FizzBuzz");
-    else
-        if(i%3==0)
-		  printf("Fizz");
+	if(i%3==0 && i%5==0)
+      		printf("FizzBuzz");
+    	else
+        	if(i%3==0)
+			printf("Fizz");
 		else
-		    if(i%5==0)
-		        printf("Buzz");
-		    else
-		       printf("%d",i);
+		    	if(i%5==0)
+		        	printf("Buzz");
+		    	else
+		       		printf("%d",i);
 }
 		
 
