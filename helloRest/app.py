7ada43d83c2664be0516ac9df94d6fbf52ce54c9
Akshay Jain <akshayjain.ecb@gmail.com>
2019-06-04 11:47:09 +0200

    Added logic to return aws instance id in headers
    Added logic to return aws instance id in headers

--- helloRest/app.py
+++ helloRest/app.py
@@ -1,11 +1,26 @@
+#Webservice to return AWS Instance ID
+#Author. Akshay.Jain
 from flask import Flask
+from flask import jsonify
+import requests
+
+#Fetch current AWS instance ID
+response = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
+instance_id = response.text
+
 app = Flask(__name__)
 
+@app.route('/api/aws',methods=['GET'])
+def hello_world():
+    #return jsonify(message)
+	resp = jsonify(
+		message="Hello World!",
+		sender= "DevOps Team"
+    )
+	resp.headers.set('aws-instance-id', response.text)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
-
+	return resp
+	
+	
 if __name__ == '__main__':
     app.run()
-
