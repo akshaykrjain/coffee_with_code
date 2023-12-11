5e70261fb1624fc40a81c5eb7a6560e7f1efe018
Akshay Jain <akshayjain.ecb@gmail.com>
2023-12-11 17:01:56 +0100

    Update counter.py improve logging

--- python/dockerized-python/counter.py
+++ python/dockerized-python/counter.py
@@ -3,15 +3,23 @@ import time
 import random
 
 # Configure logging
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 counter = 1
 
 while True:
-    logging.info("hello..#" + str(counter))
+    if counter <= 90:
+        log_level = logging.INFO
+    elif 90 < counter <= 95:
+        log_level = logging.WARNING
+    else:
+        log_level = logging.ERROR
+
+    logging.log(log_level, "hello..#" + str(counter))
     counter += 1
     if counter > 100:
         counter = 1
+
     # Generate a random sleep duration between 1 to 5 seconds
     sleep_duration = random.uniform(1, 5)
     time.sleep(sleep_duration)
