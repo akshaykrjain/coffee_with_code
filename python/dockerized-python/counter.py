70f5b16d13ee24f618702d0d62d0f812dda3d62f
ajain_nepgroup <ajain@nepgroup.com>
2024-01-25 15:36:46 +0100

    Logging in Json format

--- python/dockerized-python/counter.py
+++ python/dockerized-python/counter.py
@@ -1,9 +1,24 @@
 import logging
 import time
 import random
+import json
 
-# Configure logging
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+# Function to generate random Docker-like container names
+def generate_user_name():
+    adjectives = ["nostalgic", "focused", "vibrant", "serene", "mystic"]
+    animals = ["swan", "turing", "giraffe", "elephant", "parrot"]
+    return f"{random.choice(adjectives)}_{random.choice(animals)}"
+
+
+# Configure logging with JSON formatter
+logging.basicConfig(format="%(message)s", level=logging.INFO)
+log_formatter = logging.Formatter(
+    '{"timestamp": "%(asctime)s", "level": "%(levelname)s", "user": "%(user)s", "message": "%(message)s"}'
+)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(log_formatter)
+logging.getLogger().addHandler(console_handler)
 
 counter = 1
 
@@ -15,7 +30,12 @@ while True:
     else:
         log_level = logging.ERROR
 
-    logging.log(log_level, "hello..#" + str(counter))
+    user_name = generate_user_name()
+    log_message = f"hello from my awesome app..#{counter}"
+
+    # Log message with container name
+    logging.log(log_level, log_message, extra={"user": user_name})
+
     counter += 1
     if counter > 100:
         counter = 1
